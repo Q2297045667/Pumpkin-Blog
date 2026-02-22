66a8dc06e0fc8901bb80d829e98647238f5c705e
RB007 <36972202+RoosterBooster007@users.noreply.github.com>
2026-02-22 01:24:54 -0500

    fix: it actually works now 🎉

--- app/components/content/Youtube.vue
+++ app/components/content/Youtube.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-defineProps<{ videoId: string }>();
+defineProps<{ videoid: string }>();
 </script>
 
 <template>
     <div class="not-prose mx-auto max-w-3xl w-full aspect-video overflow-hidden rounded-lg">
-        <iframe
-            :src="`https://www.youtube.com/embed/${videoId}`"
-            class="h-full w-full"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-        />
+        <ClientOnly>
+            <iframe
+                :src="`https://www.youtube.com/embed/${videoid}`"
+                class="h-full w-full"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            />
+        </ClientOnly>
     </div>
 </template>
